40dab04c282d87f3c221ba0a6600b51460dcbe39
nsatish94 <117615816+nsatish94@users.noreply.github.com>
2025-11-27 23:59:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_GwWpmlU35', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1h1shj6rod2ma4np33qouhvs6i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3pv77vf77rt0joqc0d8nvm0oa0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
